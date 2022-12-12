5e33dfb86c75f4b8362ba623ac9e92e979fbef3a
seldenleonard <selden222@gmail.com>
2022-12-12 12:03:42 -0500

    creates Album model with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_12_165815) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_12_170206) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.string "title"
+    t.date "release_date"
+    t.integer "tracks_count"
+    t.integer "tracks_count_seconds"
+    t.integer "artist_id"
+    t.integer "publisher_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
