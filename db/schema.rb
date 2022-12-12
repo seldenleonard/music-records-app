8a7f730d25098416a33b18196a24d9d82087d6c4
seldenleonard <selden222@gmail.com>
2022-12-12 11:50:40 -0500

    creates Label model with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_12_163545) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_12_164906) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "labels", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.string "country"
+    t.boolean "independant"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "records", force: :cascade do |t|
     t.string "title"
