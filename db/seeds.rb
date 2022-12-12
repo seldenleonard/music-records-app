2591b747ae306053d51922bad04b59e8d1032353
seldenleonard <selden222@gmail.com>
2022-12-12 12:20:07 -0500

    creates label seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+labels = Label.create([{ name: "Fools Gold Records", city: "New York", country: "USA", independant: true }, { name: "Def Jam Recordings", city: "New York", country: "USA", independant: false }, { name: "Sony Music Entertainment", city: "Los Angeles", country: "USA", independant: false }, { name: "Warner Music Group", city: "Los Angeles", country: "USA", independant: false }])
