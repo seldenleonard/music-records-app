5197936e8444303a45ddfc2d3b019b21d8a7c87c
seldenleonard <selden222@gmail.com>
2022-12-12 12:06:14 -0500

    creates feature model with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_12_170206) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_12_170523) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_12_170206) do
     t.string "country"
     t.text "bio"
     t.integer "label_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "features", force: :cascade do |t|
+    t.integer "artist_id"
+    t.integer "record_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
