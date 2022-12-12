a4c0fa01ee562b40f3a8b19e6f8ae77cd149da9b
seldenleonard <selden222@gmail.com>
2022-12-12 11:59:40 -0500

    migrates publisher model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_12_165350) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_12_165815) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_12_165350) do
     t.string "city"
     t.string "country"
     t.boolean "independant"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "publishers", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.string "state"
+    t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
