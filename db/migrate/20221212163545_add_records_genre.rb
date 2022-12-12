6e66bf12c16a1854d4cc4244d1acfa9e342f2439
seldenleonard <selden222@gmail.com>
2022-12-12 11:43:19 -0500

    successfully adds genre column to record model with migration

--- db/migrate/20221212163545_add_records_genre.rb
+++ db/migrate/20221212163545_add_records_genre.rb
@@ -1,5 +1,5 @@
-class AddGenreToRecords < ActiveRecord::Migration[7.0]
+class AddRecordsGenre < ActiveRecord::Migration[7.0]
   def change
-    add_column :genre, :string
+    add_column :records, :genre, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_12_161907) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_12_163545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_12_161907) do
     t.integer "album_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "genre"
   end
 
 end
