5dae53ac007318eeb53215be6ec3ac1a3298f516
seldenleonard <selden222@gmail.com>
2022-12-12 12:52:37 -0500

    removes state attribute from publishers model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_12_170523) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_12_175057) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,7 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_12_170523) do
   create_table "publishers", force: :cascade do |t|
     t.string "name"
     t.string "city"
-    t.string "state"
     t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
